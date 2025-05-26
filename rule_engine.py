09a6527fc8bb330a2683d8496df4f340be6f4750
hannesfox <johannes.gschwendtner@gmx.at>
2025-05-26 10:45:02 +0200

    plan überarbeitet

--- rule_engine.py
+++ rule_engine.py
@@ -95,8 +95,8 @@ def find_prc_path_by_rules(feature_type_lower: str | None, ocr_all_results: dict
          lambda res, dia, bbox_b, tief, bbox_l, kl_r:
             tief is not None and 0.0 <= tief <= 40.0 and
             kl_r is not None and any(abs(kl_r - val) < 1e-6 for val in [5.0]) and
-            bbox_l is not None and 40.1 <= bbox_l <= 2000.0 and
-            bbox_b is not None and 60.1 <= bbox_b <= 2000.0,
+            bbox_l is not None and 40.0 <= bbox_l <= 2000.0 and
+            bbox_b is not None and 40.0 <= bbox_b <= 2000.0,
          (r"01_Plan-Aussen-Fase-Tasche", "05")),
 
         # Plan 42-62 16 20
@@ -104,8 +104,8 @@ def find_prc_path_by_rules(feature_type_lower: str | None, ocr_all_results: dict
          lambda res, dia, bbox_b, tief, bbox_l, kl_r:
          tief is not None and 0.0 <= tief <= 40.0 and
          kl_r is not None and any(abs(kl_r - val) < 1e-6 for val in [10.0, 0.2, 0.0]) and
-         bbox_l is not None and 40.1 <= bbox_l <= 2000.0 and
-         bbox_b is not None and 60.1 <= bbox_b <= 2000.0,
+         bbox_l is not None and 40.0 <= bbox_l <= 2000.0 and
+         bbox_b is not None and 40.0 <= bbox_b <= 2000.0,
          (r"01_Plan-Aussen-Fase-Tasche", "01")),
 
         # Plan 42-62 16x52 20
@@ -113,8 +113,8 @@ def find_prc_path_by_rules(feature_type_lower: str | None, ocr_all_results: dict
          lambda res, dia, bbox_b, tief, bbox_l, kl_r:
             tief is not None and 40.01 <= tief <= 52.0 and
             kl_r is not None and 0.0 <= kl_r <= 20.0 and
-            bbox_l is not None and 40.1 <= bbox_l <= 2000.0 and
-            bbox_b is not None and 60.1 <= bbox_b <= 2000.0,
+            bbox_l is not None and 40.0 <= bbox_l <= 2000.0 and
+            bbox_b is not None and 40.0 <= bbox_b <= 2000.0,
          (r"01_Plan-Aussen-Fase-Tasche", "02")),
 
         # Plan  10er fräser
@@ -132,7 +132,7 @@ def find_prc_path_by_rules(feature_type_lower: str | None, ocr_all_results: dict
          tief is not None and 0.0 <= tief <= 40.5 and
          kl_r is not None and 0.0 <= kl_r <= 10.0 and
          bbox_l is not None and 40.1 <= bbox_l <= 180.0 and
-         bbox_b is not None and 0.0 <= bbox_b <= 60.0,
+         bbox_b is not None and 0.0 <= bbox_b <= 40.0,
          (r"01_Plan-Aussen-Fase-Tasche", "04")),
 
 
